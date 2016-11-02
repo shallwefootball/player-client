433a7e6fe85604cbc9e95bf1f168d5bb40e3bef4
amos <amos@shallwefootball.com>
2016-11-02 23:33:20 +0900

    retinajs추가 (향후 - window.onload 로직은 개선해야 한다.)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import retina from 'retinajs'
 
 import { initStore } from '../store'
 
@@ -29,6 +30,10 @@ export default class Index extends Component {
   constructor(props) {
     super(props)
     this.store = initStore(reducers, props.initialState, props.isServer)
+  }
+
+  componentDidMount() {
+    window.onload = () => (retina())
   }
 
   render() {
