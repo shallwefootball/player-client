575ed83555cdcc61e3a58cc789ee8dd9aa3705ed
amos <amos@shallwefootball.com>
2016-11-04 01:07:29 +0900

    radium추가, match-header엘리먼트에서 컴포넌트로 변경

--- components/match-header.js
+++ components/match-header.js
@@ -1,54 +1,84 @@
-import React from 'react'
+import React, { Component } from 'react'
 import moment from 'moment'
+import radium from 'radium'
+import Button from 'react-bootstrap/lib/Button'
 
 import config from '../config'
 
 const url = config.dev.apiUrl
 
-const flag = () => (<i className="fa fa-flag" aria-hidden="true"></i>)
+class MatchHeader extends Component {
 
-export default ({
-  matchName,
-  kickoffTime,
-  homeClubName,
-  homeImageS,
-  awayClubName,
-  awayImageS,
-  homeScore,
-  awayScore,
-  note
-}) => {
+  handleClickHeader() {
 
-  note = note == '기권' ? flag() : ''
-  return (
-    <span>
-      <span className="text-info">
-        {matchName}
+    this.props.onClick(this.props.match.matchId)
+  }
+
+  _renderWithdrawFlag(note) {
+
+    return note == '기권' ? <i className="fa fa-flag" aria-hidden="true"></i> : ''
+  }
+
+  render() {
+    let {
+      matchName,
+      kickoffTime,
+      homeClubName,
+      homeImageS,
+      awayClubName,
+      awayImageS,
+      homeScore,
+      awayScore,
+      note
+    } = this.props.match
+
+    return (
+      <span
+        style={{
+          ':hover': {
+            textDecoration: 'none',
+            cursor: 'pointer'
+          }
+        }}
+        onClick={this.handleClickHeader.bind(this)}
+      >
+        <span className="text-info">
+          {matchName}
+        </span>
+        {" "}
+        <span className="text-muted">
+          {moment(kickoffTime).format('M.D')}
+        </span>
+        {" "}
+        {this._renderWithdrawFlag(note)}
+        <span style={{padding: '0 9px'}}>
+          <img
+            src={url + '/' + (homeImageS || 'default.png') }
+            style={{marginBottom: 2}}
+            data-rjs="3"
+          />
+          {" "}
+          {homeClubName}
+          {" "}
+          {homeScore} : {awayScore}
+          {" "}
+          {awayClubName}
+          {" "}
+          <img
+            src={url + '/' + (awayImageS || 'default.png')}
+            style={{marginBottom: 2}}
+            data-rjs="3"
+          />
+        </span>
+
+        <Button bsStyle="default" bsSize="xsmall">
+          참가
+        </Button>
+
       </span>
-      {" "}
-      <span className="text-muted">
-        {moment(kickoffTime).format('M.D')}
-      </span>
-      {" "}
-      {note}
-      {" "}
-      <img
-        src={url + '/' + (homeImageS || 'default.png') }
-        style={{marginBottom: 2}}
-        data-rjs="3"
-      />
-      {" "}
-      {homeClubName}
-      {" "}
-      {homeScore} : {awayScore}
-      {" "}
-      {awayClubName}
-      {" "}
-      <img
-        src={url + '/' + (awayImageS || 'default.png')}
-        style={{marginBottom: 2}}
-        data-rjs="3"
-      />
-    </span>
-  )
+    )
+  }
 }
+
+
+export default radium(MatchHeader)
--- components/match.js
+++ components/match.js
@@ -1,32 +1,59 @@
 import React, { Component } from 'react'
 
 import Accordion from 'react-bootstrap/lib/Accordion'
+import PanelGroup from 'react-bootstrap/lib/PanelGroup'
 import Panel from 'react-bootstrap/lib/Panel'
 
-import matchHeader from './match-header'
+import MatchHeader from './match-header'
 
 export default class Match extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      activeKey: null
+    }
+  }
+
+  handleSelect() {
+    console.log('onSelect')
+  }
+
+  handleClickHeader(activeKey) {
+    this.setState({ activeKey: activeKey })
+  }
 
   render() {
     return (
 
-      <Accordion>
+      <PanelGroup
+        accordion
+        activeKey={this.state.activeKey}
+        onSelect={this.handleSelect.bind(this)}
+      >
 
         {
           this.props.match.matches.map(match => {
 
             return (
               <Panel
-                header={matchHeader(match)}
+                header={
+                  <MatchHeader
+                    match={match}
+                    onClick={this.handleClickHeader.bind(this)}
+                  />
+                }
                 eventKey={match.matchId}
                 key={match.matchId}
+                collapsible
               >
                 hihihi
               </Panel>
             )
           })
         }
-      </Accordion>
+      </PanelGroup>
     )
   }
 }
