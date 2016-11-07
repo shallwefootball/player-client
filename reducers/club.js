f84a595ebb2d5387dcbb83de6cbf547869f32260
amos <amos@shallwefootball.com>
2016-11-08 02:05:41 +0900

    club, league리듀서 수정

--- reducers/club.js
+++ reducers/club.js
@@ -1,18 +1,17 @@
 import CONST from '../constraint'
 
 const initState = {
-  leagues: [],
-  seasons: []
+  clubs: []
 }
 
 export default (state = initState, action) => {
 
   switch(action.type) {
 
-    case CONST.SET_LEAGUES: {
-      const { leagues } = action
-      return { leagues}
+    case CONST.SET_CLUBS: {
+      return { clubs: action.clubs }
     }
+
     default: return state
   }
 }
--- reducers/index.js
+++ reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux'
 import league from './league'
 import match from './match'
 import team from './team'
+import club from './club'
 
 export default combineReducers({
   league,
   match,
-  team
+  team,
+  club
 })
