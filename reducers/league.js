5cadfcf0c057b4acf956b66806b76f22d236726f
Amos Dong-Keon Kim <amos@shallwefootball.com>
2016-11-18 20:06:30 +0900

    league map에러 수정

--- reducers/league.js
+++ reducers/league.js
@@ -11,8 +11,8 @@ export default (state = initState, action) => {
   switch(action.type) {
 
     case CONST.SET_LEAGUES: {
-      const { leagues } = action
-      return { leagues }
+      state.leagues = action.leagues
+      return { ...state }
     }
     case CONST.SET_LEAGUE_RANK: {
       state.ranks = action.ranks
